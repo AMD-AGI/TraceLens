4a0674d9246a103ba0e3b79338c3cbf4c5818de8
ajassani <ajassani@amd.com>
2025-01-22 19:24:58 +0000

    refactor perf model for OOP

--- TreePerf/torch_op_mapping.py
+++ TreePerf/torch_op_mapping.py
@@ -1,47 +1,21 @@
-from perf_model import *
+import perf_model
 from param_details import *
 
-op_to_param_details_func_map = {
-    'aten::linear': get_param_details_aten_linear,
-    'aten::mm': get_param_details_aten_mm,
-    'aten::addmm': get_param_details_aten_addmm,
-    'FlashAttnFunc': get_param_details_flash_attention,
-    'aten::conv2d': get_param_details_aten_conv2d,
-    'aten::conv3d': get_param_details_aten_conv3d,
-}
+# op_to_param_details_func_map = {
+#     'aten::linear': get_param_details_aten_linear,
+#     'aten::mm': get_param_details_aten_mm,
+#     'aten::addmm': get_param_details_aten_addmm,
+#     'FlashAttnFunc': get_param_details_flash_attention,
+#     'aten::conv2d': get_param_details_aten_conv2d,
+#     'aten::conv3d': get_param_details_aten_conv3d,
+# }
 
-op_to_flops_func_map = {
-    'aten::linear': linear_flops,
-    'aten::mm': linear_flops,
-    'aten::addmm': linear_flops,
-    'FlashAttnFunc': sdpa_flops,
-    'aten::conv2d': conv2d_flops,
-    'aten::conv3d': conv3d_flops,
-}
+op_to_perf_model_class_map = {
+    'aten::linear': perf_model.aten_linear,
+    'aten::mm': perf_model.aten_mm,
+    'aten::addmm': perf_model.aten_addmm,
+    'FlashAttnFunc': perf_model.flash_attention,
+    'aten::conv2d': perf_model.aten_conv,
+    'aten::conv3d': perf_model.aten_conv,
 
-op_to_bwd_flops_func_map = {
-    'aten::linear': linear_bwd_flops,
-    'aten::mm': linear_bwd_flops,
-    'aten::addmm': linear_bwd_flops,
-    'FlashAttnFunc': sdpa_bwd_flops,
-    'aten::conv2d': conv2d_bwd_flops,
-    'aten::conv3d': conv3d_bwd_flops,
-}
-
-op_to_bytes_func_map = {
-    'aten::linear': linear_bytes,
-    'aten::mm': linear_bytes,
-    'aten::addmm': linear_bytes,
-    'FlashAttnFunc': fa_bytes,
-    # 'aten::conv2d': conv2d_bytes,
-    # 'aten::conv3d': conv3d_bytes,
-}
-
-op_to_bwd_bytes_func_map = {
-    'aten::linear': linear_bwd_bytes,
-    'aten::mm': linear_bwd_bytes,
-    'aten::addmm': linear_bwd_bytes,
-    # 'FlashAttnFunc': fa_bwd_bytes,
-    # 'aten::conv2d': conv2d_bwd_bytes,
-    # 'aten::conv3d': conv3d_bwd_bytes,
 }
