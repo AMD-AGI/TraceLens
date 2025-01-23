7990f6b90df3c105ae361113d961cdc949dc802f
ajassani <ajassani@amd.com>
2025-01-22 17:06:57 -0800

    remove param details from op mapping file

--- TreePerf/torch_op_mapping.py
+++ TreePerf/torch_op_mapping.py
@@ -1,14 +1,4 @@
 import perf_model
-from param_details import *
-
-# op_to_param_details_func_map = {
-#     'aten::linear': get_param_details_aten_linear,
-#     'aten::mm': get_param_details_aten_mm,
-#     'aten::addmm': get_param_details_aten_addmm,
-#     'FlashAttnFunc': get_param_details_flash_attention,
-#     'aten::conv2d': get_param_details_aten_conv2d,
-#     'aten::conv3d': get_param_details_aten_conv3d,
-# }
 
 op_to_perf_model_class_map = {
     'aten::linear': perf_model.aten_linear,
