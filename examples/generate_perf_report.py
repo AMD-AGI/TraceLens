3b1c0a6cd242f8a6ed1b37add6a25d6606462cf4
Adeem Jassani <ajassani@amd.com>
2025-03-25 20:34:22 +0000

    update generate report for elementwise

--- examples/generate_perf_report.py
+++ examples/generate_perf_report.py
@@ -25,24 +25,40 @@ def main():
         'CONV': ['aten::convolution'],
     }
 
+    unary_elemwise_op_names = [
+        'aten::copy', 'aten::copy_',
+        'atem::clamp_min', 'aten::clamp_min_', 
+        'aten::sigmoid',
+    ]
+
+    binary_elemwise_op_names = [
+        'aten::div', 'aten::div_',
+        'aten::mul', 'aten::mul_',
+        'aten::add', 'aten::add_',
+        'aten::sigmoid_backward',
+        'aten::threshold_backward',
+    ]
+
+    op_category_to_op_name_map['UNARY_ELEMWISE'] = unary_elemwise_op_names
+    op_category_to_op_name_map['BINARY_ELEMWISE'] = binary_elemwise_op_names
+
     # Dictionary to hold the op-specific DataFrames
     op_dfs = {}
 
     for op_cat, op_names in op_category_to_op_name_map.items():
         # Filter events belonging to the current category
         op_events = [event for event in perf_analyzer.tree.events if event['name'] in op_names]
-        op_events_uids = [event['UID'] for event in op_events]
 
-        if op_cat == 'GEMM':
+        if op_cat in ['GEMM', 'UNARY_ELEMWISE', 'BINARY_ELEMWISE']:
             # For GEMM: create a single table that covers both fwd and bwd.
-            df_ops = perf_analyzer.build_df_perf_metrics(op_events_uids, bwd=False, non_data_mov=True)
+            df_ops = perf_analyzer.build_df_perf_metrics(op_events, bwd=False, non_data_mov=True)
             df_ops = perf_analyzer.summarize_df_perf_metrics(df_ops, ['mean'])
             op_dfs[op_cat] = df_ops
         else:
             # For FLASH_ATTN and CONV: create separate tables for forward and backward passes.
-            df_ops_fwd = perf_analyzer.build_df_perf_metrics(op_events_uids, bwd=False, non_data_mov=True)
+            df_ops_fwd = perf_analyzer.build_df_perf_metrics(op_events, bwd=False, non_data_mov=True)
             df_ops_fwd = perf_analyzer.summarize_df_perf_metrics(df_ops_fwd, ['mean'])
-            df_ops_bwd = perf_analyzer.build_df_perf_metrics(op_events_uids, bwd=True, non_data_mov=True)
+            df_ops_bwd = perf_analyzer.build_df_perf_metrics(op_events, bwd=True, non_data_mov=True)
             df_ops_bwd = perf_analyzer.summarize_df_perf_metrics(df_ops_bwd, ['mean'])
             op_dfs[f"{op_cat}_fwd"] = df_ops_fwd
             op_dfs[f"{op_cat}_bwd"] = df_ops_bwd
