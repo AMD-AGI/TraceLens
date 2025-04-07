9d79fb1dd2ef37a4ea6a5ca0a301e60c3815e7ef
Gabe Weisz <gabe.weisz@amd.com>
2025-04-07 08:21:12 -0500

    add licenses

--- TraceLens/PerfModel/torch_op_mapping.py
+++ TraceLens/PerfModel/torch_op_mapping.py
@@ -1,4 +1,25 @@
-# import perf_model
+# MIT License
+
+# Copyright (c) 2024 - 2025 Advanced Micro Devices, Inc. All rights reserved.
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 from . import perf_model
 
 op_to_perf_model_class_map = {
@@ -13,7 +34,7 @@ op_to_perf_model_class_map = {
 
 unary_elemwise_ops = [
     'aten::copy', 'aten::copy_',
-    'atem::clamp_min', 'aten::clamp_min_', 
+    'atem::clamp_min', 'aten::clamp_min_',
     'aten::sigmoid',
 ]
 
