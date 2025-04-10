186eef895dd71d3a3d623d517f2ffd673e57fba8
Gabe Weisz <gabe.weisz@amd.com>
2025-04-10 11:14:41 -0500

    fix potential leak

--- TraceLens/TreePerf/jax_analyses.py
+++ TraceLens/TreePerf/jax_analyses.py
@@ -131,26 +131,20 @@ class JaxAnalyses:
 
         event_key=str.join('|', JaxAnalyses.communication_events_map.keys())
         pattern = re.compile(f"^.*value:.*({event_key})\.?([\d]+)?.*size=(\d+).*: ([a-zA-Z\d].*)\[.*$")
-<<<<<<< HEAD
-        for line in open(xla_file_name, "r"):
-            m=pattern.search(line)
-            if m:
-                communication_events[m.group(1)].append([m.group(2), m.group(3), m.group(4)])
-=======
         with open(xla_file_name, "r") as f:
             for line in f:
                 m=pattern.search(line)
                 if m:
                     communication_events[m.group(1)].append([m.group(2), m.group(3), m.group(4)])
->>>>>>> eec26ca1f9ad1272db6b8969550032e40272f539
         return communication_events
 
     # filename here is the regular XLA file, not the "after-buffer-assignment" file
     @staticmethod
     def process_gemm_events_from_xla_dump(xla_file_name: str) -> dict:
         op_processor = XlaOperatorProcessor()
-        for line in open(xla_file_name, "r"):
-            op_processor.process_line(line) # communication events might also return buffers that need to be processed
+        with open(xla_file_name, "r") as f:
+            for line in f:
+                op_processor.process_line(line) # communication events might also return buffers that need to be processed
         return op_processor.process_gemm_ops()
 
     # this function only takes the minimum of each instance of the communication across all steps
