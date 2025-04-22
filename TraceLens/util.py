d929a05be98407e40ab9ab3712f88eb0634aa33d
Gabe Weisz <gabe.weisz@amd.com>
2025-04-22 08:04:57 -0500

    fix bug with save preprocessed

--- TraceLens/util.py
+++ TraceLens/util.py
@@ -7,12 +7,10 @@ class DataLoader:
             from tensorboard_plugin_profile.convert import raw_to_tool_data as convert
             data, _ = convert.xspace_to_tool_data([filename_path], "trace_viewer@^", {})
             data = data.decode("utf-8") # we get bytes back from the call above
-            return json.loads(data)
         elif filename_path.endswith('json.gz'):
             import gzip
             with gzip.open(filename_path, 'r') as fin:
                 data = fin.read().decode('utf-8')
-            return json.loads(data)
         elif filename_path.endswith('json'):
             with open(filename_path, 'r') as fin:
                 data = fin.read()
