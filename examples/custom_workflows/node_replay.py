60c07edd094c2e79a3b42d2ed40833d450c899a1
root <rony.leppanen@amd.com>
2025-05-02 17:30:22 +0000

    snapshot, first version gen perf report + node replay microbench + readme

---
+++ examples/custom_workflows/node_replay.py
@@ -0,0 +1,508 @@
+"""
+Using TraceLens, and .json traces produced by torch profiler, this script provides a comparison on
+how much the workload-specific metrics of most important GEMMs and CONVS
+differ when microbenchmarking the same ops using torch and low-level benchmarking tools.
+Currently works with hipblaslt-bench and MIOpenDriver, if they are available in the environment.
+Tested with main branch @ 8f40079.
+
+Note that the trace .json files are located recursively - this script does not depend on a certain directory structure.
+
+Example directory tree:
+
+profiling_fa_cudnn
+    results_013_profile_nv_cudnn
+        traces
+            huvideo_traces_rank_0_step_3.json
+            ...
+            huvideo_traces_rank_7_step_3.json
+
+    results_013_profile_nv_fa
+        traces
+            huvideo_traces_rank_0_step_3.json
+            ...
+            huvideo_traces_rank_7_step_3.json
+    results_013_profile_rocm_fa
+        traces
+            huvideo_traces_rank_0_step_3.json
+            ...
+            huvideo_traces_rank_7_step_3.json
+
+Example usage:
+
+Analyze all .json trace files containing rocm_fa and step_3 in their full path.
+
+Trace files include rank number using the pattern rank_0, rank_1, ..., rank_n.
+Script uses this pattern to recognize rank numbers, thus we give -p rank_
+
+python analyze_traces_tracelens.py -b profiling_fa_cudnn -f rocm_fa step_3
+
+Targeting:
+
+profiling_fa_cudnn\results_013_profile_rocm_fa\traces\huvideo_traces_rank_0_step_3.json
+profiling_fa_cudnn\results_013_profile_rocm_fa\traces\huvideo_traces_rank_1_step_3.json
+profiling_fa_cudnn\results_013_profile_rocm_fa\traces\huvideo_traces_rank_2_step_3.json
+profiling_fa_cudnn\results_013_profile_rocm_fa\traces\huvideo_traces_rank_3_step_3.json
+profiling_fa_cudnn\results_013_profile_rocm_fa\traces\huvideo_traces_rank_4_step_3.json
+profiling_fa_cudnn\results_013_profile_rocm_fa\traces\huvideo_traces_rank_5_step_3.json
+profiling_fa_cudnn\results_013_profile_rocm_fa\traces\huvideo_traces_rank_6_step_3.json
+profiling_fa_cudnn\results_013_profile_rocm_fa\traces\huvideo_traces_rank_7_step_3.json
+
+NOTE: add -d flag for launching a dry run (check if correct files will be targeted).
+      add -e flag for specifying extension. json and gz are currently supported, json is used by default
+"""
+import argparse
+import gc
+import os
+import subprocess
+import time
+
+import os.path as osp
+import pandas as pd
+
+from typing import Dict, List, Callable, Optional, Tuple, Union, Any
+
+from TraceLens import TreePerfAnalyzer, EventReplayer
+
+from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import get_context
+
+from perf_report_configs import gemm_ops, conv_ops
+from perf_report_utils import parse_traces, collect_df_perf_metrics_per_group
+
+
+summarize_df_perf_metrics = TreePerfAnalyzer.summarize_df_perf_metrics
+
+group2ops = {
+    "gemm": gemm_ops,
+    "conv": conv_ops,
+}
+
+df_cols_gemm = [
+    "param: M",
+    "param: N",
+    "param: K",
+    "param: bias",
+    "param: stride_A",
+    "param: stride_B",
+    "param: dtype_A_B",
+    "param: transpose",
+    "kernel_names_first",
+]
+
+df_cols_conv = [
+    "param: convNd",
+    "param: input_shape",
+    "param: filter_shape",
+    "param: dtype_input_weight",
+    "param: input_stride",
+    "param: weight_stride",
+    "param: bias",
+    "param: stride",
+    "param: padding",
+    "param: dilation",
+    "param: transposed_conv",
+    "param: output_padding",
+    "param: groups",
+    "kernel_names_first"
+]
+
+df_cols_common = [
+    "name",
+    "Kernel Time (µs)_mean",
+    "kernel_time_sum_cum_pct",
+]
+
+group2dfcols = {
+    "gemm": df_cols_common + df_cols_gemm,
+    "conv": df_cols_common + df_cols_conv,
+}
+
+
+def benchmark_func(
+    func: Callable,
+    device: str,
+    envvars: Optional[Dict[str, str]] = None,
+    warmup: int = 1000,
+    active_steps: int = 1000
+) -> Optional[float]:
+    """
+    Benchmark a function with warmup and average steps.
+    Disclaimer: This method would be innacurate for very short ops.
+    Args:
+        func (callable): The function to benchmark.
+        device (str): GPU device.
+        envvars (dict): Pass environment variables for possible logging purposes.
+        warmup (int): Number of warmup iterations.
+        active_steps (int): Number of iterations to average over.
+    Returns:
+        float: Average time taken per iteration in microseconds.
+    """
+    import torch
+
+    if envvars:
+        for key, val in envvars.items():
+            os.environ[key] = val
+
+        if "MIOPEN_ENABLE_LOGGING_CMD" in envvars:
+            # MIOpen writes to stderr using C/C++ system calls (instead of Python's sys.stderr object)
+            # So we need to redirect at the system level:
+            # Save original stderr
+            # Read and write permission, create file if required, truncate to zero length if file exists
+            # Redirect stderr at OS level
+            # Restore stderr and clean up
+            orig_stderr_fd = os.dup(2)
+            log_fd = os.open(envvars["MIOPEN_LOG_FILE"], os.O_RDWR | os.O_CREAT | os.O_TRUNC, 0o644)
+            os.dup2(log_fd, 2)
+            func()
+            os.dup2(orig_stderr_fd, 2)
+            os.close(orig_stderr_fd)
+            os.close(log_fd)
+        else:
+            func()
+
+        return None
+
+    start = torch.cuda.Event(enable_timing=True)
+    end = torch.cuda.Event(enable_timing=True)
+
+    # Warmup phase
+    for _ in range(warmup):
+        func()
+
+    # Benchmarking phase
+    torch.cuda.synchronize(device)
+
+    start.record()
+
+    for _ in range(active_steps):
+        func()
+
+    end.record()
+    torch.cuda.synchronize(device)
+
+    elapsed_time_ms = start.elapsed_time(end)
+    avg_time_ms = elapsed_time_ms / active_steps
+    avg_time_us = avg_time_ms * 1e3
+
+    torch.cuda.empty_cache()
+
+    return avg_time_us
+
+
+def benchmark_func_wrapper(args: Tuple) -> Optional[float]:
+    avg_time_us = benchmark_func(*args)
+    return avg_time_us
+
+
+def run_subprocess_cmd(cmd: Union[str, List[str]], shell: bool = False) -> Optional[str]:
+    """Run a subprocess command and return the stdout as string"""
+    try:
+        result = subprocess.run(
+            cmd,
+            shell=shell,
+            capture_output=True,
+            text=True,
+            check=True
+        )
+        return result.stdout
+    except subprocess.CalledProcessError as e:
+        print(f"Subprocess error: {e}")
+        return None
+
+
+def get_logging_envvars(group: str, i: int) -> Dict[str, str]:
+    logging_envvars = None
+
+    if group == "gemm":
+        logging_envvars = {
+            "HIPBLASLT_LOG_MASK": "32",
+            "HIPBLASLT_LOG_FILE": f"./hipblaslt-bench-{i}.log",
+        }
+
+    elif group == "conv":
+        logging_envvars = {
+            "MIOPEN_ENABLE_LOGGING_CMD": "1",
+            "MIOPEN_LOG_FILE": f"./miopen-driver-{i}.log"
+        }
+
+    return logging_envvars
+
+
+def prepare_hipblaslt_bench_cmd(log_file: str):
+    """Prepare hipblaslt-bench command for microbenchmarking."""
+    modify = [
+        [r"s/--algo_method index/--algo_method heuristic/g", log_file],
+        [r"s/--solution_index [0-9]*/--requested_solution 1/", log_file],
+        [r"s/--cold_iters [0-9]*/--cold_iters 100/g", log_file],
+        [r"s/--iters [0-9]*/--iters 100/g", log_file],
+        [r"s/--rotating [0-9]*/--rotating 512/g", log_file],
+        [r"s/$/ --flush --initialization trig_float --use_gpu_timer --print_kernel_info/", log_file],
+    ]
+
+    for mod in modify:
+        _ = run_subprocess_cmd(["sed", "-i"] + mod)
+
+
+def prepare_miopen_driver_cmd(log_file: str):
+    """Prepare hipblaslt-bench command for microbenchmarking."""
+    modify = [
+        [r"/\[LogCmdConvolution\]/!d", log_file],
+        [r"s/.*\.\/bin\///", log_file],
+    ]
+
+    for mod in modify:
+        _ = run_subprocess_cmd(["sed", "-i"] + mod)
+
+
+def run_single_hipblaslt_bench(log_file: str) -> float:
+    """Run single hipblaslt-bench command and return microseconds from output."""
+    with open(log_file, "r") as file:
+        hipblaslt_bench_cmd = file.read().strip()
+
+    result_stdout = run_subprocess_cmd(hipblaslt_bench_cmd, shell=True) # shell=True is a potential security risk (!)
+    result_stdout_lines = result_stdout.split("\n")
+    target_line = result_stdout_lines[-5]
+    # ...,hipblaslt-Gflops,hipblaslt-GB/s,us
+    bench_time_mean = float(target_line.split(",")[-1])
+
+    return bench_time_mean
+
+
+def run_single_miopen_driver(log_file: str) -> float:
+    """Run single MIOpenDriver command and return microseconds from output"""
+    with open(log_file, "r") as file:
+        miopen_driver_cmd = file.read().strip()
+
+    result_stdout = run_subprocess_cmd(miopen_driver_cmd, shell=True) # shell=True is a potential security risk (!)
+    result_stdout_lines = result_stdout.split("\n")
+    target_line = result_stdout_lines[-3]
+    # ..., GFLOPs, GB/s, timeMs
+    bench_time_mean = float(target_line.split(",")[-1]) * 1e3
+
+    return bench_time_mean
+
+
+def run_microbenchmarking(group: str, logging_envvars: Dict[str, str]) -> float:
+    if group == "gemm":
+        # Modify hipblaslt-bench.log command for microbenchmarking
+        log_file = logging_envvars["HIPBLASLT_LOG_FILE"]
+        prepare_hipblaslt_bench_cmd(log_file)
+
+        # hipblaslt-bench time
+        bench_time_mean = run_single_hipblaslt_bench(log_file)
+
+    elif group == "conv":
+        # Modify miopen-driver.log command for microbenchmarking
+        log_file = logging_envvars["MIOPEN_LOG_FILE"]
+        prepare_miopen_driver_cmd(log_file)
+
+        # MIOpenDriver time
+        bench_time_mean = run_single_miopen_driver(log_file)
+
+    return bench_time_mean
+
+
+def run_node_replay(group, df_ops_all_summary):
+    df_results = None
+    device = "cuda"
+    args_cols = ["Input Dims_first", "Input type_first", "Input Strides_first", "Concrete Inputs_first"]
+
+    for i in range(len(df_ops_all_summary)):
+        event = dict(args=dict())
+        row = dict(df_ops_all_summary.iloc[i])
+
+        for col in row.keys():
+            if col in args_cols:
+                event["args"][col.split("_")[0]] = row[col]
+            else:
+                event[col] = row[col]
+
+        replayer = EventReplayer(event, device="cuda")
+
+        # Get group-specific logging environment variables
+        logging_envvars = get_logging_envvars(group, i)
+
+        # Log hipblaslt-bench/MIOpenDriver commands (pass envvars, no warmup, 1 active step)
+        with ProcessPoolExecutor(max_workers=1, mp_context=get_context("spawn")) as executor:
+            args = [(replayer.replay, device, logging_envvars, 0, 1)]
+            _ = list(executor.map(benchmark_func_wrapper, args))
+
+        # Run microbenchmarking with hipblaslt-bench/MIOpenDriver
+        bench_time_mean = run_microbenchmarking(group, logging_envvars)
+
+        # Node replay time (no envvars, 1000 warmup and 1000 active steps)
+        with ProcessPoolExecutor(max_workers=1, mp_context=get_context("spawn")) as executor:
+            args = [(replayer.replay, device, None, 1000, 1000)]
+            result = list(executor.map(benchmark_func_wrapper, args))
+
+        replay_time_mean = result[0]
+
+        # Workload time from profile trace
+        profile_time_mean = row["Kernel Time (µs)_mean"]
+
+        # Record results
+        diff_profile = replay_time_mean - profile_time_mean
+        diff_bench = bench_time_mean - profile_time_mean
+
+        percent_diff_profile = (replay_time_mean - profile_time_mean) / profile_time_mean * 100
+        percent_diff_bench = (bench_time_mean - profile_time_mean) / profile_time_mean * 100
+
+        result_dict = {
+            f"{group} no.": i,
+            f"Avg. {group}-bench time (us)": bench_time_mean,
+            "Avg. replay time (us)": replay_time_mean,
+            "Avg. profile time (us)": profile_time_mean,
+            "Diff profile vs. replay (us)": diff_profile,
+            f"Diff profile vs. {group}-bench (us)": diff_bench,
+            "Diff profile vs. replay (pct)": percent_diff_profile,
+            f"Diff profile vs. {group}-bench (pct)": percent_diff_bench,
+        }
+
+        df_results = pd.concat([df_results, pd.DataFrame([result_dict])]) if df_results is not None else pd.DataFrame([result_dict])
+
+        del replayer
+        gc.collect()
+
+        time.sleep(1)
+
+
+def run_node_replay(group, df_ops_summary):
+    if df_ops_summary is None:
+        print(f"No events to replay from group {group}")
+        return
+
+    df_ops_summary_99pct = df_ops_summary[df_ops_summary["kernel_time_sum_cum_pct"] < 0.99]
+
+    if df_ops_summary_99pct.empty:
+        df_ops_summary_99pct = df_ops_summary
+
+    print(df_ops_summary_99pct.loc[:, group2dfcols[group]].to_string(max_colwidth=75))
+
+    df_results = None
+    device = "cuda"
+    args_cols = ["Input Dims_first", "Input type_first", "Input Strides_first", "Concrete Inputs_first"]
+
+    for i in range(len(df_ops_summary_99pct)):
+        event = dict(args=dict())
+        row = dict(df_ops_summary_99pct.iloc[i])
+
+        for col in row.keys():
+            if col in args_cols:
+                event["args"][col.split("_")[0]] = row[col]
+            else:
+                event[col] = row[col]
+
+        replayer = EventReplayer(event, device="cuda")
+
+        # Get group-specific logging environment variables
+        logging_envvars = get_logging_envvars(group, i)
+
+        # Log hipblaslt-bench/MIOpenDriver commands (pass envvars, no warmup, 1 active step)
+        with ProcessPoolExecutor(max_workers=1, mp_context=get_context("spawn")) as executor:
+            args = [(replayer.replay, device, logging_envvars, 0, 1)]
+            _ = list(executor.map(benchmark_func_wrapper, args))
+
+        # Run microbenchmarking with hipblaslt-bench/MIOpenDriver
+        bench_time_mean = run_microbenchmarking(group, logging_envvars)
+
+        # Node replay time (no envvars, 1000 warmup and 1000 active steps)
+        with ProcessPoolExecutor(max_workers=1, mp_context=get_context("spawn")) as executor:
+            args = [(replayer.replay, device, None, 1000, 1000)]
+            result = list(executor.map(benchmark_func_wrapper, args))
+
+        replay_time_mean = result[0]
+
+        # Workload time from profile trace
+        profile_time_mean = row["Kernel Time (µs)_mean"]
+
+        # Record results
+        diff_profile = replay_time_mean - profile_time_mean
+        diff_bench = bench_time_mean - profile_time_mean
+
+        percent_diff_profile = (replay_time_mean - profile_time_mean) / profile_time_mean * 100
+        percent_diff_bench = (bench_time_mean - profile_time_mean) / profile_time_mean * 100
+
+        result_dict = {
+            f"{group} no.": i,
+            f"Avg. {group}-bench time (us)": bench_time_mean,
+            "Avg. replay time (us)": replay_time_mean,
+            "Avg. profile time (us)": profile_time_mean,
+            "Diff profile vs. replay (us)": diff_profile,
+            f"Diff profile vs. {group}-bench (us)": diff_bench,
+            "Diff profile vs. replay (pct)": percent_diff_profile,
+            f"Diff profile vs. {group}-bench (pct)": percent_diff_bench,
+        }
+
+        df_results = pd.concat([df_results, pd.DataFrame([result_dict])]) if df_results is not None else pd.DataFrame([result_dict])
+
+        del replayer
+        gc.collect()
+
+        time.sleep(1)
+
+    df_results.set_index(f"{group} no.", inplace=True)
+    return df_results
+
+
+def run_standalone_node_replay(base_dirpath, ext="json", include_only=["rank_0"], dry_run=False):
+    all_traces_grouped = parse_traces(base_dirpath, ext, include_only)
+
+    if all_traces_grouped is None:
+        return
+
+    for parent_dirpath, filenames in all_traces_grouped.items():
+        dfs_all = {group: None for group in group2ops}
+        prefix = "_".join(include_only)
+        xlsx_path = osp.join(parent_dirpath, f"{prefix}_node_replay_report.xlsx")
+
+        print(f"==================== Creating node replay report ====================")
+        print(f"Parent directory: {parent_dirpath}")
+        print(f"Excel file (dummy): {osp.basename(xlsx_path)}")
+        print(f"Filters: {', '.join(include_only)}")
+        print("Traces:")
+        print(*filenames, sep="\n")
+
+        if dry_run:
+            print("==================== End of dry run ====================")
+            continue
+
+        if osp.exists(xlsx_path):
+            print(f"Excel file already exists: ({xlsx_path})")
+            print("Terminating...")
+            return
+
+        for filename in filenames:
+            filepath = osp.join(parent_dirpath, filename)
+
+            print(f"Starting TreePerfAnalyzer with {filename}")
+            perf_analyzer = TreePerfAnalyzer.from_file(filepath)
+
+            # Collect group-specific perf metrics from single rank
+            dfs_per_group = collect_df_perf_metrics_per_group(perf_analyzer, group2ops)
+            for group, df in dfs_per_group.items():
+                dfs_all[group] = pd.concat([dfs_all[group], df]) if dfs_all[group] is not None else df
+
+            del perf_analyzer
+            gc.collect()
+
+        for group, df_ops in dfs_all.items():
+            df_ops_summary = summarize_df_perf_metrics(df_ops, agg_metrics=["mean", "std"])
+            df_ops_summary["kernel_time_sum_cum_pct"] = df_ops_summary["Kernel Time (µs)_sum"].cumsum() / df_ops_summary["Kernel Time (µs)_sum"].sum()
+            df_results = run_node_replay(group, df_ops_summary)
+            print(df_results.to_string())
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Parse and summarize traces produced by torch profiler using TraceLens.")
+    parser.add_argument("-b", type=str, required=True, help="Path to base directory which contains profiling experiments as subdirectories.")
+    parser.add_argument("-e", type=str, default="json", help="Extension to use for identifying trace files. json and gz are supported.")
+    parser.add_argument("-f", type=str, nargs='+', default=["rank_0"], help="Select files containing given substring(s) in their name.")
+    parser.add_argument("-d", action="store_true", help="Dry run for checking if correct trace paths found.")
+
+    args = parser.parse_args()
+
+    run_standalone_node_replay(args.b, args.e, args.f, args.d)
+
+
+if __name__ == "__main__":
+    main()
---
+++ examples/custom_workflows/perf_report_configs.py
@@ -0,0 +1,85 @@
+# These ops are used for calculating detailed performance metrics for these ops
+# https://github.com/AMD-AIG-AIMA/TraceLens/blob/main/TraceLens/PerfModel/torch_op_mapping.py
+gemm_ops = [
+    "aten::mm", "aten::_scaled_mm", "aten::addmm", "aten::bmm", "aten::baddbmm",
+]
+
+conv_ops = [
+    "aten::convolution",
+]
+
+# TBA:
+# "aten::_efficient_attention_forward",
+# "aten::_scaled_dot_product_attention_math",
+# "aten::_scaled_dot_product_efficient_attention",
+# "aten::_scaled_dot_product_flash_attention",
+# "flash_attn::_flash_attn_varlen_forward",
+attn_ops = [
+    "FlashAttnFunc",
+    "flash_attn::_flash_attn_forward",
+    "aten::_scaled_dot_product_cudnn_attention",
+]
+
+unary_elemwise_ops = [
+    "aten::copy", "aten::copy_",
+    "aten::clamp_min", "aten::clamp_min_",
+    "aten::sigmoid",
+]
+
+binary_elemwise_ops = [
+    "aten::div", "aten::div_",
+    "aten::mul", "aten::mul_",
+    "aten::add", "aten::add_",
+    "aten::sigmoid_backward",
+    "aten::threshold_backward",
+]
+
+group2ops = {
+    "gemm": gemm_ops,
+    "conv": conv_ops,
+    "attn": attn_ops,
+    "un_elemwise": unary_elemwise_ops,
+    "bin_elemwise": binary_elemwise_ops,
+}
+
+# In addition to gemm and elemwise ops,
+# these launchers are used for constructing high-level grouped breakdown from the kernel launchers summary
+conv_ops_launchers = [
+    "aten::miopen_convolution", "aten::cudnn_convolution",
+]
+
+pad_ops_launchers = [
+    "aten::replication_pad3d",
+]
+
+upsample_ops_launchers = [
+    "aten::upsample_nearest3d", "aten::upsample_nearest2d",
+]
+
+norm_ops_launchers = [
+    "aten::native_group_norm",
+]
+
+attn_ops_launchers = [
+    "aten::_scaled_dot_product_cudnn_attention",
+    "flash_attn::_flash_attn_forward",
+    "flash_attn::_flash_attn_varlen_forward",
+    "aten::_efficient_attention_forward",
+]
+
+concat_ops_launchers = [
+    "aten::cat",
+]
+
+all_ops_launchers = gemm_ops + conv_ops_launchers + pad_ops_launchers + upsample_ops_launchers + norm_ops_launchers + attn_ops_launchers + unary_elemwise_ops + binary_elemwise_ops + concat_ops_launchers
+
+grouped_breakdown_mapping = {
+    "Attention": attn_ops_launchers,
+    "GEMM": gemm_ops,
+    "Conv": conv_ops_launchers,
+    "Norm": norm_ops_launchers,
+    "UpSample": upsample_ops_launchers,
+    "Pad": pad_ops_launchers,
+    "Elemwise": unary_elemwise_ops + binary_elemwise_ops,
+    "Concat": concat_ops_launchers
+}
