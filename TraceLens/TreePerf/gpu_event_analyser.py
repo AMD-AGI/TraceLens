ad227755ff31eaf16ee6aa9671cff3226959d762
Gabe Weisz <gabe.weisz@amd.com>
2025-05-05 10:33:33 -0400

    remove unwanted change

--- TraceLens/TreePerf/gpu_event_analyser.py
+++ TraceLens/TreePerf/gpu_event_analyser.py
@@ -265,7 +265,7 @@ class JaxGPUEventAnalyser(GPUEventAnalyser):
                         event['t_end'] = event['ts'] + event['dur']
                     cur_dict[GPUEventAnalyser.all_gpu_key].append(event)
                     name = event.get('name')
-                    if (any(name.lower().startswith(x) for x in ['copy', 'memcpy'])):
+                    if (any(name.lower().startswith(x) for x in ['copy', 'memcpy', 'memset'])):
                         cur_dict[GPUEventAnalyser.memcpy_key].append(event)
                     elif name.startswith('nccl'):
                         cur_dict[GPUEventAnalyser.communication_key].append(event)
