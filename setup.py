e0ffe821c25a92a583dc7f9d09cde0bde0f02a80
Ossi Lehtikangas <ossi.lehtikangas@amd.com>
2025-06-04 06:18:14 +0000

    fix:

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     name='TraceLens',
     version='0.1.0',
     packages=find_packages(where='.'),  # Will pick up 'TraceLens' automatically
+    package_dir={"": "."},
     install_requires=[
         'pandas',
         'tqdm',
