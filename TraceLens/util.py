f70f4c48e4b2caf80c68d5dea7b34ec4819b6b30
Ossi Lehtikangas <ossi.lehtikangas@amd.com>
2025-08-28 12:30:15 +0000

    feat: add more CK FABwdKeys

--- TraceLens/util.py
+++ TraceLens/util.py
@@ -245,7 +245,7 @@ class TraceEventUtils:
 
         # keywords for splitting jax events
         GemmKeys = ["Cijk", "gemm", "nvjet", "cublasLt"]
-        FABwdKeys = ["FmhaBwd", "flash_bprop"]
+        FABwdKeys = ["FmhaBwd", "flash_bprop", "ck_fused_attn::dk_dv_reduce_thd"]
         FAFwdKeys = ["FmhaFwd", "flash_fprop"]
         FAV3Keys = ["kernel_func"] # find a more precise way to do this
         ConvKeys = ["FillBuffer", "conv_", "conv."]
