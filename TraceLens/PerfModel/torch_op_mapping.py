069716b8517ec73cadf6eaf507a2220b3b6b3236
Gyula Zakor <Gyula.Zakor@amd.com>
2025-11-10 03:35:02 -0600

    Fix record_param_comms categorization

--- TraceLens/PerfModel/torch_op_mapping.py
+++ TraceLens/PerfModel/torch_op_mapping.py
@@ -139,11 +139,13 @@ def categorize_torch_op(row):
             return "SDPA_fwd"
     elif row["name"].startswith("triton"):
         return "triton"
+    elif row["name"].startswith("record_param_comms"):
+        return "record_param_comms"
     if "kernel_details" in row and len(row["kernel_details"]) > 0:
         kernel_name = row["kernel_details"][0]["name"]
     else:
         raise ValueError(
-            "Row does not contain 'kernel_names' or 'kernel_details' with a valid name."
+            f"Row does not contain 'kernel_names' or 'kernel_details' with a valid name. Row: {row}"
         )
     if kernel_name.startswith("void at::native"):
         if debug:
