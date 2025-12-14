c92ae2d3ba8b0306fc42b6b188e6d1b3d1950bcb
ajassani <ajassani@amd.com>
2025-12-14 17:00:06 +0000

    lint: fix linting error

--- TraceLens/PerfModel/torch_op_mapping.py
+++ TraceLens/PerfModel/torch_op_mapping.py
@@ -143,10 +143,10 @@ def categorize_torch_op(row):
         return "record_param_comms"
     if "kernel_details" in row and len(row["kernel_details"]) > 0:
         kernel_name = row["kernel_details"][0]["name"]
-    # else:
-    #     raise ValueError(
-    #         f"Row does not contain 'kernel_names' or 'kernel_details' with a valid name. Row: {row}"
-    #     )
+        # else:
+        #     raise ValueError(
+        #         f"Row does not contain 'kernel_names' or 'kernel_details' with a valid name. Row: {row}"
+        #     )
         if kernel_name.startswith("void at::native"):
             if debug:
                 print("Found ATen native kernel:", kernel_name[:64])
