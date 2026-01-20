3020c8fc0d4291d11b05fc4a978fc0eda7df5aaf
Tharun Adithya Srikrishnan <tsrikris@amd.com>
2026-01-20 22:53:48 +0000

    Update custom_workflows: trace_annotation

--- examples/custom_workflows/split_vllm_trace_annotation.py
+++ examples/custom_workflows/split_vllm_trace_annotation.py
@@ -1,4 +1,9 @@
-#!/usr/bin/env python3
+###############################################################################
+# Copyright (c) 2024 - 2025 Advanced Micro Devices, Inc. All rights reserved.
+#
+# See LICENSE for license information.
+###############################################################################
+ 
 """
 Split a vLLM trace into per-iteration or per-dummy-run traces.
 
