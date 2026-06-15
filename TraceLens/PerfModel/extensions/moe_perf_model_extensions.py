7008a74706bd0c4af86b9f2ba2c8a044b7bf5cac
Deval Shah <devashah@amd.com>
2026-06-15 00:23:42 -0500

    fix precision of fp8 moe

--- TraceLens/PerfModel/extensions/moe_perf_model_extensions.py
+++ TraceLens/PerfModel/extensions/moe_perf_model_extensions.py
@@ -374,7 +374,7 @@ class moe_aiter_fused_blockscale(FusedMoE):
 
     def get_compute_precision(self):
         dtype = self.param_details.get("input_dtype")
-        return torch_dtype_map(dtype) if dtype else None
+        return "fp8" if dtype == "fp8" or dtype == "bf16" else torch_dtype_map(dtype)
 
     def get_maf_type(self):
         return "matrix"
