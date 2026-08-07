7abc20f7e32c2f2365877d68bf863c87e6b2f967
Kyle Hoffmeyer <Kyle.Hoffmeyer@amd.com>
2026-08-07 17:56:14 -0400

    moved categorization

--- TraceLens/PerfModel/torch_op_mapping.py
+++ TraceLens/PerfModel/torch_op_mapping.py
@@ -272,14 +272,6 @@ op_to_perf_model_class_map.update(get_pseudo_op_mappings())
 # Add pseudo-op category-only mappings (no perf model, just a category label).
 CATEGORY_ONLY_OP_MAPPING.update(get_pseudo_op_category_only_mappings())
 
-replication_pad_ops = [
-    "aten::replication_pad1d",
-    "aten::replication_pad2d",
-    "aten::replication_pad3d",
-]
-for op in replication_pad_ops:
-    op_to_perf_model_class_map[op] = perf_model.aten_replication_pad
-
 unary_elemwise_ops = [
     "aten::copy",
     "aten::copy_",
@@ -372,6 +364,12 @@ reduce_ops = [
     "aten::amax",
 ]
 
+replication_pad_ops = [
+    "aten::replication_pad1d",
+    "aten::replication_pad2d",
+    "aten::replication_pad3d",
+]
+
 for op in unary_elemwise_ops:
     op_to_perf_model_class_map[op] = perf_model.aten_unary_elementwise
 for op in binary_elemwise_ops:
@@ -380,6 +378,8 @@ for op_name, op_class in norm_ops.items():
     op_to_perf_model_class_map[op_name] = op_class
 for op in reduce_ops:
     op_to_perf_model_class_map[op] = perf_model.aten_reduce
+for op in replication_pad_ops:
+    op_to_perf_model_class_map[op] = perf_model.aten_replication_pad
 
 # ---------------------------------------------------------------------------
 # Pattern-based matchers for perf models with generated kernel names.
