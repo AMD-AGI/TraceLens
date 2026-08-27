df75ecc7699d5d63650ee3bc95e6c963ec377764
Daniel Holanda <holand.daniel@gmail.com>
2026-08-27 09:57:50 -0700

    headers

--- TraceLens/Agent/Analysis/skills/analysis-orchestrator/evals/hooks.py
+++ TraceLens/Agent/Analysis/skills/analysis-orchestrator/evals/hooks.py
@@ -1,6 +1,8 @@
+###############################################################################
 # Copyright (c) 2026 Advanced Micro Devices, Inc. All rights reserved.
 #
 # See LICENSE for license information.
+###############################################################################
 
 """TraceLens setup and scoring for the `gemm-01-repeatability` behavior case.
 
